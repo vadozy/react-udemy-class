8f9da1ef223de1925b5931811667d1b82fa4de0e
Vadim Storozhuk <vadim.storo@gmail.com>
2018-06-03 20:13:00 -0400

    section 05 (before adding Radium)

---
+++ section-05-styling/src/App.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: 'person_id_01', name: 'Max', age: 28 },
+      { id: 'person_id_02', name: 'Manu', age: 29 },
+      { id: 'person_id_03', name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
+  
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice(); // slice() creates a copy -- good practice
+    const persons = [...this.state.persons]; // same as above
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
+  nameChangedHandler = (value, id) => {
+    const persons = [...this.state.persons]; // copy
+    const person = persons.find(p => p.id === id); // should be a copy instead?
+    person.name = value;
+    this.setState({persons: persons});
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+  render() {
+
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((p, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              changed={event => this.nameChangedHandler(event.target.value, p.id)}
+              name={p.name}
+              age={p.age} 
+              key={p.id} />
+          })}
+        </div>
+      );
+
+      style.backgroundColor = 'red';
+    }
+
+    const classes = []
+    if (this.state.persons.length <=2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <=1) {
+      classes.push('bold');
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button 
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+
+        {persons}
+
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
+
+}
+
+export default App;
