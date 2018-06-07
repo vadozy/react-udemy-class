79e37a808878033544facce8813d215a298116e6
Vadim Storozhuk <vadim.storozhuk@numeric.com>
2018-06-07 16:01:22 -0400

    section 08 (wip) Dynamic Ingredients added, before building controls

--- section-08-burger-builder/src/components/Burger/Burger.js
+++ section-08-burger-builder/src/components/Burger/Burger.js
@@ -4,11 +4,32 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+	// Vadim's solution
+	//let ings = [];
+	//for (let ing in props.ingredients) {
+	//	for (let i = 0; i < props.ingredients[ing]; i++) {
+	//		ings.push( <BurgerIngredient type={ing} key={ings.length} /> );
+	//	}
+	//}
+
+	//Author's solution
+	let ings = Object.keys(props.ingredients)
+		.map(igKey => {
+			return  [...Array(props.ingredients[igKey])].map((_, i) => {
+				return <BurgerIngredient type={igKey} key={igKey + i} />;
+			});
+		})
+		.reduce((arr, el) => [...arr, ...el], []); // flattening the array of 4 arrays
+
+	if (ings.length === 0) {
+		ings = <p>Please start adding ingredients!</p>;
+	}
+
 	return (
 		<div className={classes.Burger} >
 			<BurgerIngredient type="bread-top" />
-			<BurgerIngredient type="cheese" />
-			<BurgerIngredient type="meat" />
+			{ings}
 			<BurgerIngredient type="bread-bottom" />
 		</div>
 	);
