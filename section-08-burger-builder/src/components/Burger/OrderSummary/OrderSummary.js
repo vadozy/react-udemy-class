af0f6f9c1f04e3e018ac5f1fc0ad69dc96ca3e7c
Vadim Storozhuk <vadim.storozhuk@numeric.com>
2018-06-08 16:36:41 -0400

    section 08 (wip) added Esc button handling to the Order Summary (had to turn it into component)

--- section-08-burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ section-08-burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = props => {
+class OrderSummary extends Component {
 
-	const ingredientSummary = Object.keys(props.ingredients)
-		.map(igKey => {
-			return <li key={igKey} ><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}</li>
-		});
+	constructor(props){
+		super(props);
+		this.escFunction = this.escFunction.bind(this);
+	}
 
-	return (
-		<Aux>
-			<h3>Your Order</h3>
-			<p>A delicious burger with the following ingredients:</p>
-			<ul>
-				{ingredientSummary}
-			</ul>
-			<p>Continue to Checkout?</p>
-			<Button clicked={props.purchaseCancelled} btnType='Danger'>CANCEL</Button>
-			<Button clicked={props.purchaseContinued} btnType='Success'>CONTINUE</Button>
-		</Aux>
-	);
-};
+	escFunction(event){
+		if(event.keyCode === 27) {
+			this.props.purchaseCancelled();
+		}
+	}
 
-export default orderSummary;
+	componentDidMount() {
+		document.addEventListener("keydown", this.escFunction, false);
+	}
+
+	componentWillUnmount(){
+		document.removeEventListener("keydown", this.escFunction, false);
+	}
+
+	render() {
+		const ingredientSummary = Object.keys(this.props.ingredients)
+			.map(igKey => {
+				return <li key={igKey} ><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}</li>
+			});
+
+		return (
+			<Aux>
+				<h3>Your Order</h3>
+				<p>A delicious burger with the following ingredients:</p>
+				<ul>
+					{ingredientSummary}
+				</ul>
+				<p><strong>Total Price: {this.props.totalPrice.toFixed(2)}</strong></p>
+				<p>Continue to Checkout?</p>
+				<Button clicked={this.props.purchaseCancelled} btnType='Danger'>CANCEL</Button>
+				<Button clicked={this.props.purchaseContinued} btnType='Success'>CONTINUE</Button>
+			</Aux>
+		);
+	}
+}
+
+export default OrderSummary;
