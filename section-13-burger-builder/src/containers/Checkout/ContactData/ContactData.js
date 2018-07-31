bef71f6e9118e91f9e52780fb21f1a2e8121500e
Vadim Storozhuk <vadim.storo@gmail.com>
2018-07-30 23:29:47 -0400

    section 13 (wip) End of Lecture 225

---
+++ section-13-burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+
+import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import classes from './ContactData.css';
+
+import axios from '../../../axios-orders';
+
+import Input from '../../../components/UI/Input/Input';
+
+class ContactData extends Component {
+	state = {
+		orderForm: {
+			name: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Your Name'
+				},
+				value: ''
+			},
+			street: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Street'
+				},
+				value: ''
+			},
+			zip: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'ZIP Code'
+				},
+				value: ''
+			},
+			country: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Country'
+				},
+				value: ''
+			},
+			email: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'email',
+					placeholder: 'Your E-Mail'
+				},
+				value: ''
+			},
+			deliveryMethod: {
+				elementType: 'select',
+				elementConfig: {
+					options: [
+						{value: 'fastest', displayValue: 'Fastest'},
+						{value: 'cheapest', displayValue: 'Cheapest'}
+					]
+				},
+				value: ''
+			}
+		},
+		loading: false
+	}
+
+	orderHandler = event => {
+		event.preventDefault();
+		console.log(this.props.ingredients);
+
+		this.setState({loading: true});
+		const order = {
+			ingredients: this.props.ingredients,
+			price: this.props.totalPrice // should be computed on the server, where products are stored
+		};
+		axios.post('/orders.json', order) // .json is the firebase requirement
+			.then(response => {
+				console.log(response);
+				this.setState({loading: false});
+				this.props.history.push('/');
+			})
+			.catch(error => {
+				console.log("Something went wrong %s", error)	;
+				this.setState({loading: false});
+			});
+		
+	}
+
+	render () {
+		const formElementsArray = [];
+		for (let key in this.state.orderForm) {
+			formElementsArray.push({
+				id: key,
+				config: this.state.orderForm[key]
+			});
+		}
+
+		let form = (
+			<div className={classes.ContactData}>
+				<h4>Enter your Contact Data</h4>
+				<form>
+					{formElementsArray.map(formElement => (
+						<Input
+							key={formElement.id}
+							elementType={formElement.config.elementType}
+							elementConfig={formElement.config.elementConfig}
+							value={formElement.config.value} />
+					))}
+					<Button btnType="Success" clicked={this.orderHandler} >Order</Button>
+				</form>
+			</div>
+		);
+
+		if (this.state.loading) {
+			form = <Spinner />
+		}
+		return form;
+	}
+}
+
+export default ContactData;
