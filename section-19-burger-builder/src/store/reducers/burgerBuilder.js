c8224207707c980b0b67de4b9f38092fb6357502
Vadim Storozhuk <vadim.storo@gmail.com>
2018-09-29 15:30:25 -0400

    section 19 (Improving Burger Project) started. This is the exact copy of "section-18-burger-builder" at its end, tagged "section-18"

---
+++ section-19-burger-builder/src/store/reducers/burgerBuilder.js
@@ -0,0 +1,68 @@
+/*
+ * Reducer for building an order
+ */
+import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+
+const initialState = {
+    ingredients: null,
+    totalPrice: 4, // base proce
+    error: false,
+    building: false // set true when burger building process started
+};
+
+const addIngredient = (state, action) => {
+    const updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 };
+    const updatedIngs = updateObject(state.ingredients, updatedIng);
+    const updatedState = {
+        ingredients: updatedIngs,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+        building: true
+    };
+    return(updateObject(state, updatedState));
+};
+
+const removeIngredient = (state, action) => {
+    const updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 };
+    const updatedIngs = updateObject(state.ingredients, updatedIng);
+    const updatedState = {
+        ingredients: updatedIngs,
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
+        building: true
+    };
+    return(updateObject(state, updatedState));
+};
+
+const setIngredient = (state, action) => {
+    return updateObject(state, {
+        ingredients: action.ingredients,
+        totalPrice: 4,
+        error: false,
+        building: false
+    });
+};
+
+const fetchIngredientFailed = (state, action) => {
+    return updateObject(state, {
+        error: true
+    });
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.ADD_INGREDIENT: return addIngredient(state, action);
+        case actionTypes.REMOVE_INGREDIENT: return removeIngredient(state, action);
+        case actionTypes.SET_INGREDIENTS: return setIngredient(state, action);
+        case actionTypes.FETCH_INGREDIENTS_FAILED: return fetchIngredientFailed(state, action);
+        default: return state;
+    }
+};
+
+export default reducer;
