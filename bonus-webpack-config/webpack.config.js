9064319988da6795ee55c79e0e5ac19b11993a6e
Vadim Storozhuk <vadim.storo@gmail.com>
2018-10-07 20:02:04 -0400

    section 22 after lecture 369

---
+++ bonus-webpack-config/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map', // allows to debug orig code in browser
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: ''
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'] // try to find files with this extension if no extension is given
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_module/,
+                use: [ // use is like loader with additional config
+                    {loader: 'style-loader'}, // order of style-loader and css-loader is important
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1, // informs cssloader that postcss-loader runs before it
+                            modules: true,
+                            localIdentName: '[name]__[local]__[hash:base64:5]'
+                        }
+                    }, // webpack parses and applies loaders from right to left
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [
+                                autoprefixer({
+                                    browsers: [
+                                        "> 1%",
+                                        "last 2 versions"
+                                    ]
+                                })
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
