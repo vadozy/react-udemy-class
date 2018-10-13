28c1966c9473b210623ff31a6517858f70715683
Vadim Storozhuk <vadim.storo@gmail.com>
2018-10-13 12:51:21 -0400

    section 23 (Bonus Next.js) after lecture 383.

--- bonus-next-js/pages/auth/index.js
+++ bonus-next-js/pages/auth/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import User from '../../components/User';
+
 const authIndexPage = () => (
     <div>
         <h1>The Auth Index Page</h1>
+        <User name="Vadim" age={49} />
+        {/* Next can use styled-jsx as follows: */}
+        <style jsx>{`
+            div {
+                border: 1px solid #eee;
+                box-shadow: 0 2p 3px #ccc;
+                padding: 20px;
+                text-align: center;
+            }
+        `}</style>
     </div>
 );
 
