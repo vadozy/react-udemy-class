07d9b6867e0415051cdca2f1da9aaacd39c38c99
Vadim Storozhuk <vadim.storo@gmail.com>
2018-10-13 19:14:02 -0400

    section 24 (Bonus Animations) after lecture 388 (no animations yet)

--- bonus-animations/src/App.js
+++ bonus-animations/src/App.js
@@ -6,13 +6,27 @@ import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
 class App extends Component {
+  state = {
+    modalIdOpen: false
+  }
+
+  showModal = () => {
+    this.setState({modalIdOpen: true});
+  }
+
+  closeModal = () => {
+    this.setState({modalIdOpen: false});
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal />
-        <Backdrop />
-        <button className="Button">Open Modal</button>
+        <Modal
+          show={this.state.modalIdOpen}
+          closed={this.closeModal}/>
+        <Backdrop show={this.state.modalIdOpen} />
+        <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
       </div>
