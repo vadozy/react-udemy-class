9ec16228919ffdb546fb0784e6036373b5547b76
Vadim Storozhuk <vadim.storo@gmail.com>
2018-10-13 13:20:41 -0400

    section 23 complete (Bonus Next.js)

--- bonus-next-js/pages/auth/index.js
+++ bonus-next-js/pages/auth/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import User from '../../components/User';
 
-const authIndexPage = () => (
+const authIndexPage = props => (
     <div>
-        <h1>The Auth Index Page</h1>
+        <h1>The Auth Index Page of [ {props.appName} ]</h1>
         <User name="Vadim" age={49} />
         {/* Next can use styled-jsx as follows: */}
         <style jsx>{`
@@ -17,5 +17,14 @@ const authIndexPage = () => (
         `}</style>
     </div>
 );
+
+authIndexPage.getInitialProps = context => {
+    const promise = new Promise(resolve => {
+        setTimeout(() => {
+            resolve({appName: 'Super App 2'});
+        }, 1000);
+    });
+    return promise;
+};
 
 export default authIndexPage;
--- bonus-next-js/pages/index.js
+++ bonus-next-js/pages/index.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 
-const indexPage = () => (
-    <div>
-        <h1>The Main Page</h1>
-        <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
-        <button onClick={() => Router.push('/auth')} >Go to Auth</button>
-    </div>
-);
+class IndexPage extends Component {
 
-export default indexPage;
+    //executes on the server
+    static async getInitialProps(context) {
+        console.log(context); // logs on the server
+
+        const promise = new Promise(resolve => {
+            setTimeout(() => {
+                resolve({appName: 'Super App'});
+            }, 1000);
+        });
+        return promise;
+        
+        // This works too
+        //const ret = await promise;
+        //return ret;
+
+        // This works too (synchronous)
+        //return {appName: 'Super App'};
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>The Main Page of [ {this.props.appName} ]</h1>
+                <p>Go to {" "}
+                    <Link href="/auth">
+                        <a>Auth</a>
+                    </Link></p>
+                <button onClick={() => Router.push('/auth')} >Go to Auth</button>
+            </div>
+        );
+    }
+}
+
+export default IndexPage;
