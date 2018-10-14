7128b4ec96b6678c38773427d563a17d5177924c
Vadim Storozhuk <vadim.storo@gmail.com>
2018-10-14 14:27:49 -0400

    section 24 (Bonus Animations) after lecture 398 (React more CSSTransition)

--- bonus-animations/src/components/Modal/Modal.js
+++ bonus-animations/src/components/Modal/Modal.js
@@ -16,7 +16,14 @@ const modal = props => {
             unmountOnExit
             in={props.show}
             timeout={animationTiming}
-            classNames="fade-slide">
+            classNames={{
+                enter: '',
+                enterActive: 'ModalOpen',
+                exit: '',
+                exitActive: 'ModalClosed',
+                appear: '',
+                appearActive: ''
+            }}>
                 <div className="Modal">
                     <h1>A Modal</h1>
                     <button className="Button" onClick={props.closed}>Dismiss</button>
